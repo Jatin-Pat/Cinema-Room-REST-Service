32886c94da86dfb49b743ba4c10a0205d53b4ce3
Jatin Patel <jatin_pat@outlook.com>
2023-01-14 09:53:06 -0500

    Initial spring boot starter project commit

---
+++ src/test/java/com/github/jatinpat/cinemaroomrestservice/CinemaRoomRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jatinpat.cinemaroomrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaRoomRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
