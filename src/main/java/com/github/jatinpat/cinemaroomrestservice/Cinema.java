006fdc828b0668c2c8e6fc17b7894b45eaf70650
Jatin Patel <jatin_pat@outlook.com>
2023-01-15 14:52:39 -0500

    Added Cinema.java class components

--- src/main/java/com/github/jatinpat/cinemaroomrestservice/Cinema.java
+++ src/main/java/com/github/jatinpat/cinemaroomrestservice/Cinema.java
@@ -1,4 +1,44 @@
 package com.github.jatinpat.cinemaroomrestservice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cinema {
+    private int total_rows;
+    private int total_columns;
+    List<Seat> available_seats = new ArrayList<>();
+
+    public Cinema(int total_rows, int total_columns) {
+        this.total_rows = total_rows;
+        this.total_columns = total_columns;
+        for (int i = 1; i <= total_rows; i++) {
+            for (int j = 1; j <= total_columns; j++) {
+                available_seats.add(new Seat(i, j));
+            }
+        }
+    }
+
+    public int getTotal_rows() {
+        return total_rows;
+    }
+
+    public int getTotal_columns() {
+        return total_columns;
+    }
+
+    public List<Seat> getAvailable_seats() {
+        return available_seats;
+    }
+
+    public void setTotal_rows(int total_rows) {
+        this.total_rows = total_rows;
+    }
+
+    public void setTotal_columns(int total_columns) {
+        this.total_columns = total_columns;
+    }
+
+    public void setAvailable_seats(List<Seat> available_seats) {
+        this.available_seats = available_seats;
+    }
 }
