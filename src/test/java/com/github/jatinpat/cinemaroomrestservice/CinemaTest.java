7017b9c7901e27b37fffdc849df43626418930f9
Jatin Patel <jatin_pat@outlook.com>
2023-01-15 16:26:03 -0500

    updated tests

--- src/test/java/com/github/jatinpat/cinemaroomrestservice/CinemaTest.java
+++ src/test/java/com/github/jatinpat/cinemaroomrestservice/CinemaTest.java
@@ -6,19 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CinemaTest {
     @Test
-    void contains49seats() {
+    void testContains49seats() {
         var cinema = new Cinema(9, 9);
         assertEquals(81, cinema.available_seats.size());
     }
 
     @Test
-    void seatPrice8() {
+    void testSeatPrice8() {
         var cinema = new Cinema(9, 4);
         assertEquals(8, cinema.available_seats.get(20).getPrice());
     }
 
     @Test
-    void seatPrice10() {
+    void testSeatPrice10() {
         var cinema = new Cinema(4, 1);
         assertEquals(10, cinema.available_seats.get(1).getPrice());
     }
---
+++ src/test/java/com/github/jatinpat/cinemaroomrestservice/SeatControllerTest.java
@@ -0,0 +1,15 @@
+package com.github.jatinpat.cinemaroomrestservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeatControllerTest {
+
+    @Test
+    void testEndPoint() {
+
+    }
+
+
+}
